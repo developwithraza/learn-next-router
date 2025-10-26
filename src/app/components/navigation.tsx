a1b86c5e0a24241824ffa69fbb947b3f4f7b1abb
Develop With Raza <devlopwithr@gmail.com>
2025-10-26 18:37:50 +0530

    remove product

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -6,14 +6,13 @@ import React from 'react'
 
 function Navigation() {
     const pathname = usePathname()
-    
+
     return (
         <nav>
             <Link href="/" className={pathname === '/' ? 'mx-4 font-semibold  ' : 'mx-4 font-semibold text-yellow-600 '}>Home</Link>
 
             <Link href="/about" className={pathname === '/about' ? 'mx-4 font-semibold  ' : 'mx-4 font-semibold text-yellow-600 '}>About</Link>
 
-            <Link href="/products/1" className={pathname === '/products/1' ? 'mx-4 font-semibold  ' : 'mx-4 font-semibold text-yellow-600 '}>Product1</Link>
         </nav>
     )
 }
